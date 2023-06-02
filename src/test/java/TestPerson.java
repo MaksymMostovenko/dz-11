799f899d52cc19ec62e6ace8344620cacd28383b
MaksymMostovenko <blockmaksim@gmail.com>
2023-06-02 15:30:29 +0300

    Initial comit

---
+++ src/test/java/TestPerson.java
@@ -0,0 +1,6 @@
+public class TestPerson {
+    public Person testMaleOBject = new Male("testFistName", "testSecondName", 18);
+    public Person testFealeObject = new Female("testFistName", "testSecondName", 18);
+
+
+}
